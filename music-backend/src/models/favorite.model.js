695b42ca111abe66aecd2696a666567a6c8b9ad3
LE DINH BAO <ddinhbao05@gmail.com>
2026-04-21 21:52:02 +0700

    Fix(db): table favorite

--- music-backend/src/models/favorite.model.js
+++ music-backend/src/models/favorite.model.js
@@ -2,19 +2,30 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
 const Favorite = sequelize.define(
-    "Favorite",
-    {
-
-        created_at: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW,
-        },
+  "Favorite",
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
     },
-    {
-        tableName: "favorites",
-        timestamps: true,      // Bật lên để dùng tính năng ngày tháng
-        createdAt: "created_at", // Map đúng tên cột trong database
-    }
+
+    song_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "favorites",
+    timestamps: true, // Bật lên để dùng tính năng ngày tháng
+    createdAt: "created_at", // Map đúng tên cột trong database
+  },
 );
 
 module.exports = Favorite;
