237526292666a2508e939c8464b6df36b5f7fb20
LE DINH BAO <ddinhbao05@gmail.com>
2026-04-21 21:50:39 +0700

    Fix(db): table rating

--- music-backend/src/models/rating.model.js
+++ music-backend/src/models/rating.model.js
@@ -2,38 +2,52 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
 const Rating = sequelize.define(
-    "Rating",
-    {
-        rating_id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        score: {
-            type: DataTypes.TINYINT,
-            allowNull: false,
-            validate: {
-                min: 1, // Điểm thấp nhất là 1
-                max: 5, // Điểm cao nhất là 5 
-            },
-        },
-
-        created_at: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW,
-        },
+  "Rating",
+  {
+    rating_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        tableName: "ratings",
-        timestamps: true,
-        createdAt: "created_at",
-        indexes: [
-            {
-                unique: true,
-                fields: ["user_id", "song_id"], // Đảm bảo 1 User chỉ đánh giá 1 Bài hát đúng 1 lần
-            },
-        ],
-    }
+
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    song_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    score: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
+      validate: {
+        min: 1, // Điểm thấp nhất là 1
+        max: 5, // Điểm cao nhất là 5
+      },
+    },
+
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "ratings",
+    timestamps: true,
+    createdAt: "created_at",
+    indexes: [
+      {
+        unique: true,
+        fields: ["user_id", "song_id"], // Đảm bảo 1 User chỉ đánh giá 1 Bài hát đúng 1 lần
+      },
+      {
+        fields: ["song_id"], // Thêm index để tối ưu query tìm user đã rate bài này
+      },
+    ],
+  },
 );
 
 module.exports = Rating;
