6fb98e6532ba02b7507b2a8dce980933c72487f4
LE DINH BAO <ddinhbao05@gmail.com>
2026-04-29 17:15:16 +0700

    feat: update environment configuration, Dockerfile, and remove unused files

--- Server/src/config/db.js
+++ Server/src/config/db.js
@@ -6,8 +6,8 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     logging: false,
   },
