805915e45ba953ee75d09da1c0ef517a42780a3d
mudt11 <huynhthachdinh.tpht@gmail.com>
2026-05-11 00:43:21 +0700

    deploy: update proxy config for vps

--- UI/next.config.ts
+++ UI/next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://14.225.212.97:5000/api/:path*', // Link gọi về VPS của bạn
+      },
+    ];
+  },
 };
 
 export default nextConfig;
